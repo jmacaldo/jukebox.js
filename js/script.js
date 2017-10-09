a492c271e1aa3b6ad55157f05b8dc31844400038
Joven Macaldo <jmacaldo@me.com>
2017-10-09 19:11:18 -0400

    json + dynamic player!
    mp3 player is now dynamic as it loads songs from a json playlist instead of hard coding.

--- js/script.js
+++ js/script.js
@@ -18,13 +18,13 @@ function Jukebox(artist, song, album, genre, source){
 
 //Metallica audio controls and event listeners
 
-document.getElementById("metallica").addEventListener("click", puppets);
-document.getElementById("metPause").addEventListener("click", metallicaPause);
-document.getElementById("metStop").addEventListener("click", metallicaStop);
+// document.getElementById("metallica").addEventListener("click", puppets);
+// document.getElementById("metPause").addEventListener("click", metallicaPause);
+// document.getElementById("metStop").addEventListener("click", metallicaStop);
 
 
 function puppets(){
-  var x = document.getElementById("01");
+  var x = document.getElementById("S0");
     x.play();
 }
 
@@ -41,9 +41,9 @@ function metallicaPause(){
 
 
 //Smashing pumpkins audio controls and event listeners
-document.getElementById("pumpkins").addEventListener("click", pumpkins);
-document.getElementById("pumpPause").addEventListener("click", pumpPause);
-document.getElementById("pumpStop").addEventListener("click", pumpStop);
+// document.getElementById("pumpkins").addEventListener("click", pumpkins);
+// document.getElementById("pumpPause").addEventListener("click", pumpPause);
+// document.getElementById("pumpStop").addEventListener("click", pumpStop);
 
 function pumpkins(){
   var x = document.getElementById("02");
@@ -61,9 +61,9 @@ function pumpStop(){
   }
 
 //Oasis audio controls and event listeners
-  document.getElementById("oasis").addEventListener("click", oasis);
-  document.getElementById("oasisPause").addEventListener("click", oasisPause);
-  document.getElementById("oasisStop").addEventListener("click", oasisStop);
+  // document.getElementById("oasis").addEventListener("click", oasis);
+  // document.getElementById("oasisPause").addEventListener("click", oasisPause);
+  // document.getElementById("oasisStop").addEventListener("click", oasisStop);
 
   function oasis(){
     var x = document.getElementById("03");
@@ -79,3 +79,103 @@ function pumpStop(){
       x.pause();
       x.currentTime = 0;
     }
+
+//playlist JSON
+var playList = [
+  {
+    "artist" : "Metallica",
+    "song" : "Master of Puppets",
+    "album" : "Master of Puppets",
+    "genre": "Metal"
+  },
+  {
+    "artist" : "The Smashing Pumpkins",
+    "song" : "1979",
+    "album" : "Mellon Collie and the Infinite Sadness",
+    "genre": "Alternative"
+  },
+  {
+    "artist" : "Oasis",
+    "song" : "Champagne Supernova",
+    "album" : "(What's the Story) Morning Glory?",
+    "genre": "Rock"
+  }
+]
+
+//dynamic player
+
+document.getElementById("playBtn").addEventListener("click", player);
+document.getElementById("nextBtn").addEventListener("click", next);
+document.getElementById("stopBtn").addEventListener("click", stop);
+
+let songN = 2; //zero indexed - number of songs in playlist
+var i = 0;
+
+function player(){
+    document.getElementById('player').innerHTML = ('Artist: '+ playList[i].artist +'<br>'+'Song: '+playList[i].song + '<br>'+ "Album: " + playList[i].album +'<br>'+'Genre: '+playList[i].genre);
+    var x = document.getElementById("S"+i);
+    x.play();
+  //  window[songID]++;
+    console.log('value of i from player function: '+i);
+  };//function ends here
+
+
+//plays next song in playlist
+function next(){
+
+  var x = document.getElementById("S"+i);
+  x.pause();
+  x.currentTime = 0;
+  i++;
+  console.log('value of i from next: '+i);
+  player();
+}
+
+//stops current track
+
+function stop(){
+  var x = document.getElementById("S"+i);
+    x.pause();
+    x.currentTime = 0;
+  }
+
+//unrelated test code below
+that = 0;
+
+function test(input) {
+    window[input]++;
+}
+
+test("that");
+
+console.log('value of that: '+that); // 1
+
+/* last known working snippet of dynamic player
+
+document.getElementById("playBtn").addEventListener("click", player);
+document.getElementById("nextBtn").addEventListener("click", next);
+
+let songN = 2; //zero indexed - number of songs in playlist
+var i = 0;
+function player(songID){
+    document.getElementById('player').innerHTML = ('Artist: '+ playList[i].artist +'<br>'+'Song: '+playList[i].song + '<br>'+ "Album: " + playList[i].album +'<br>'+'Genre: '+playList[i].genre);
+    var x = document.getElementById("S"+i);
+    x.play();
+    window[songID]++;
+    console.log('value of i from player function: '+i);
+
+
+
+  };//function ends here
+
+function next(){
+
+  var x = document.getElementById("S"+i);
+  x.pause();
+  x.currentTime = 0;
+  i++;
+  console.log('value of i from next: '+i);
+  player();
+}
+
+*/
