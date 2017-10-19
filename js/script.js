f98655df39bafe959921cb5400fcbbde89d40f2c
Joven Macaldo <jmacaldo@me.com>
2017-10-18 20:32:57 -0400

    jukebox.js complete!
    final polishes made. ready for submission.

--- js/script.js
+++ js/script.js
@@ -11,29 +11,12 @@ var Playlist = function (artist,song,album,genre,source,art){
 //song instances made with Playlist contructor
 var songs = [];
 
-var metallica = new Playlist ('Metallica','Master of Puppets','Master of Puppets','Metal','audio/puppets.mp3','imgs/0.jpg');
+var metallica = new Playlist ('Metallica','Master of Puppets','Master of Puppets','Metal','audio/puppets.mp3','imgs/bg0.jpg');
 songs.push(metallica);
-var pumpkins = new Playlist ('The Smashing Pumpkins','1979','Mellon Collie and the Infinite Sadness','Alternative','audio/1979.mp3','imgs/1.jpg');
+var pumpkins = new Playlist ('The Smashing Pumpkins','1979','Mellon Collie and the Infinite Sadness','Alternative','audio/1979.mp3','imgs/bg1.jpg');
 songs.push(pumpkins);
-var oasis = new Playlist ('Oasis','Champagne Supernova','(What\'s the Story) Morning Glory?','Rock','audio/oasis.mp3','imgs/2.jpg');
+var oasis = new Playlist ('Oasis','Champagne Supernova','(What\'s the Story) Morning Glory?','Rock','audio/oasis.mp3','imgs/bg2.jpg');
 songs.push(oasis);
-
-
-
-//song uploader
-var uploaded;
-
-function htmlUpload(filelist){
-  uploaded =  filelist.files[0].name;
-  var newUpload = new Playlist (''+uploaded,'','','','audio/'+uploaded,'');
-  songs.push(newUpload);
-  console.log('value of uploaded song (from inside function): ' +uploaded);
-
-    //document.getElementById("audiosource").setAttribute("src", filelist.files[0].name);
-    //console.log('value of uploaded song: ' +uploaded);
-
-}
-console.log('value of uploaded song (from global): ' +uploaded);
 
 
 
@@ -44,22 +27,41 @@ document.getElementById("playBtn").addEventListener("click", player);
 document.getElementById("nextBtn").addEventListener("click", next);
 document.getElementById("stopBtn").addEventListener("click", stop);
 
-
 //player global values
-let songN = songs.length ; //zero indexed - number of songs in playlist
 var i = 0;
-//var audioHTML;
+
+//song uploader
+function htmlUpload(filelist){
+  var uploaded =  filelist.files[0].name;
+  var newUpload = new Playlist (""+uploaded,'','','','audio/'+uploaded,'imgs/defaultBg.jpg');
+  songs.push(newUpload);
+  console.log(songs.length);
+  console.log('value of var uploaded song (from inside function): ' +uploaded);
+  i = songs.length -1;
+  player();
+
+}
+
+//load first song on page load
+
+document.getElementById('player').innerHTML = ('<span id="artist"> '+ songs[i].artist + '</span><br>'+'<span id="song"> '+songs[i].song + '</span><br>'+ '<span id="album">  ' + songs[i].album +'</span><br>'+'<span id="genre">  '+songs[i].genre+'</span><br>');
+var audioHTML = document.getElementById("idAudio");
+audioHTML.src = songs[i].source;
+document.getElementById('mainContainer').style.backgroundImage = "url(" + songs[i].art + ")";
+
 
 //play button and display artist info
 function player(){
-    document.getElementById('player').innerHTML = ('Artist: '+ songs[i].artist +'<br>'+'Song: '+songs[i].song + '<br>'+ "Album: " + songs[i].album +'<br>'+'Genre: '+songs[i].genre);
+    document.getElementById('player').innerHTML = ('<span id="artist"> '+ songs[i].artist + '</span><br>'+'<span id="song"> '+songs[i].song + '</span><br>'+ '<span id="album">  ' + songs[i].album +'</span><br>'+'<span id="genre">  '+songs[i].genre+'</span><br>');
     var audioHTML = document.getElementById("idAudio");
-
     audioHTML.src = songs[i].source;
     audioHTML.play();
-    console.log('audioHTML ->'+ audioHTML);
+    console.log('status of audioHTML: '+ audioHTML);
     console.log('value of i from player function: '+i);
+    document.getElementById('mainContainer').style.backgroundImage = "url(" + songs[i].art + ")";
   };
+
+
 
 //play next song in playlist
 function next(){
@@ -71,7 +73,7 @@ function next(){
   console.log('value of i from next: '+i);
 
 //reset playlist position to the beginning when the end is reached
-    if (i > songN) {
+    if (i > songs.length-1) {
        i = 0;
        player();
     } else {
@@ -81,6 +83,7 @@ function next(){
 //stops current track
 
 function stop(){
+  console.log('stop');
   var x = document.getElementById("idAudio");
     x.pause();
     x.currentTime = 0;
